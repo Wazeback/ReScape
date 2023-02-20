282f6bab32aadbf5d7f324a7894acd5078c8e799
Wazeback <david.lelystad@gmail.com>
2023-02-20 09:02:32 +0100

    added extra raycast and rigidbody movement

--- Assets/scrips/player/PickupController.cs
+++ Assets/scrips/player/PickupController.cs
@@ -3,133 +3,66 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 using Object = UnityEngine.Object;
+
 
 public class PickupController  : MonoBehaviour
 {
     public Transform mainCam;
-    public Image mainimage;
-    public Image secondimage;
+    public Image mainImage;
+    public Image secondImage;
     
     private GameObject obj;
     private Rigidbody objRb;
     private GameObject pickObj;
 
+    private int rayAmount = 30;
+    private float[] distances = new float[30];
+
     private bool resizingPickObj;
     
     public float dist;
 
-    private float[] distances = new float[8];
-    private Vector3[] closeHit = new Vector3[8];
+    private Vector3 direction;
 
-
-    private Vector3 center;
-
-    void FixedUpdate()
+    
+    private void FixedUpdate()
     {
-        
-        RaycastHit hitCheck;
-        bool raycastCheck = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitCheck, dist);
-        if (raycastCheck && hitCheck.transform.gameObject.CompareTag("pickup"))
-        {
-            mainimage.enabled = false;
-            secondimage.enabled = true;
-        }
-        else
-        {
-            mainimage.enabled = true;
-            secondimage.enabled = false;
-            
-        }
-        
+        HandleCursor();
         if (resizingPickObj)
-        {
-            
-            pickObj = obj;
-            Vector3[] corners = GetObjectCorners(pickObj);
-            
-            float initialDistance = Vector3.Distance(mainCam.position, pickObj.transform.position);
-            Vector3 direction = mainCam.forward;
-            
-            Bounds bounds = pickObj.GetComponent<Renderer>().bounds;
-            float offset = (bounds.size.magnitude)/2;
-            RaycastHit hit;
+            HandleResize();
 
-            int i = 0;
-            foreach (Vector3 corner in corners)
-            {
-                direction = (corner - mainCam.position).normalized;
-                {
-                    if (Physics.Raycast(mainCam.position, direction, out hit, 200.0f, ~(1 << pickObj.layer)))
-                    {
-                        distances[i] = Vector3.Distance(mainCam.position, hit.point);
-                        closeHit[i] = corner;
-                        i++;
-                    }
-
-                    Debug.DrawLine(mainCam.position, corner + direction * 200.0f, Color.red, 0.1f);
-                }
-            }
-
-            float shortestValue = Mathf.Min(distances);
-            Vector3 closestPoint = closeHit[Array.IndexOf(distances, shortestValue)];
-            
-
-            if (shortestValue >= 0) 
-            {
-                pickObj.transform.position = mainCam.position + mainCam.forward * (shortestValue-Vector3.Distance(pickObj.transform.position, closestPoint));
-                // pickObj.transform.position = pickObj.transform.position * (shortestValue - Vector3.Distance(pickObj.transform.position, closestPoint));
-                // pickObj.transform.position = closestPoint;
-                
-                float scaleFactor = Vector3.Distance(mainCam.position, pickObj.transform.position) / initialDistance;
-                pickObj.transform.localScale *= scaleFactor;  
-            }
-
-            
-        }
-        
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (obj == null)
-            {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            if (obj) 
+                dropObj();
+            else {
                 RaycastHit hit;
                 bool raycast = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, dist);
                 if (raycast)
-                {
                     PickUpObject(hit.transform.gameObject);
-                }
-                
-            }
-            else
-            {
-                dropObj();
             }
         }
-        
     }
 
-    void PickUpObject(GameObject pickObj)
+    private void PickUpObject(GameObject pickObj) 
     {
-        if (pickObj.CompareTag("pickup"))
-        {
+        if (pickObj.CompareTag("pickup")) {
             pickObj.layer = LayerMask.NameToLayer("WhatIsPickedUp");
             objRb = pickObj.GetComponent<Rigidbody>();
             objRb.useGravity = false;
             objRb.constraints = RigidbodyConstraints.FreezeAll;
             objRb.transform.parent = mainCam;
             obj = pickObj;
-
             resizingPickObj = true;
-            
         }
     }
     
-    void dropObj()
+    private void dropObj()
     {
         resizingPickObj = false;
         objRb.transform.gameObject.layer = LayerMask.NameToLayer("Default");
@@ -137,39 +70,89 @@ public class PickupController  : MonoBehaviour
         objRb.constraints = RigidbodyConstraints.None;
         objRb.transform.parent = null;
         obj = null;
-        
     }
     
-    private Vector3[] GetObjectCorners(GameObject obj)
+    private Vector3[] GetObjectPoints(GameObject obj)
     {
-        Vector3[] corners = new Vector3[8];
-        Transform objPos = obj.GetComponent<Transform>();
-        center = objPos.position;
-        // center = obj.GetComponent<Renderer>().bounds.center;
+        Vector3[] corners = new Vector3[rayAmount];
+        Transform objPos = obj.transform;
+        Vector3 center = objPos.position;
         Quaternion referenceRotation = objPos.rotation.normalized;
-
-        Debug.DrawLine(center, center+ new Vector3(0, 10, 0), Color.green);
+        Vector3 extents = obj.transform.lossyScale/2;
         
-        // Vector3 extents = obj.GetComponent<Renderer>().bounds.size/3f;
-
-        Vector3 extents = obj.GetComponent<Transform>().lossyScale/2;
-        // corners[0] = center + referenceRotation * new Vector3(extents.x, extents.y, extents.z);
-        // corners[1] = center + referenceRotation * new Vector3(extents.x, extents.y, -extents.z);
-        // corners[2] = center + referenceRotation * new Vector3(extents.x, -extents.y, extents.z);
-        // corners[3] = center + referenceRotation * new Vector3(extents.x, -extents.y, -extents.z);
-        // corners[4] = center + referenceRotation * new Vector3(-extents.x, extents.y, extents.z);
-        // corners[5] = center + referenceRotation * new Vector3(-extents.x, extents.y, -extents.z);
-        // corners[6] = center + referenceRotation * new Vector3(-extents.x, -extents.y, extents.z);
-        // corners[7] = center + referenceRotation * new Vector3(-extents.x, -extents.y, -extents.z);
-        for (UInt16 it = 0; it < 8; it++)
-        {
+        //corners of the object
+        for (UInt16 it = 0; it <= 7; it++)
             corners[it] = center + referenceRotation * new Vector3(extents.x*(1 - (4 & it)/2), extents.y*(1 - (2 & it)), extents.z*(1 - (1 & it)*2) );
-            Debug.DrawLine(center, corners[it], Color.blue);
-        }
+        
+        //center planes of tje object
+        corners[8] = center + referenceRotation * new Vector3(extents.x, 0, 0);
+        corners[9] = center + referenceRotation * new Vector3(-extents.x, 0, 0);
+        corners[10] = center + referenceRotation * new Vector3(0, extents.y, 0);
+        corners[11] = center + referenceRotation * new Vector3(0, -extents.y, 0);
+        corners[12] = center + referenceRotation * new Vector3(0, 0, extents.z);
+        corners[13] = center + referenceRotation * new Vector3(0, 0, -extents.z);
+
+        //middele of every corner of the object.
+        corners[14] = center + referenceRotation * new Vector3(extents.x, 0, extents.z);
+        corners[15] = center + referenceRotation * new Vector3(-extents.x, 0, extents.z);
+        corners[16] = center + referenceRotation * new Vector3(0, extents.y, extents.z);
+        corners[17] = center + referenceRotation * new Vector3(0, -extents.y, extents.z);
+        corners[18] = center + referenceRotation * new Vector3(extents.x, 0, -extents.z);
+        corners[19] = center + referenceRotation * new Vector3(-extents.x, 0, -extents.z);
+        corners[20] = center + referenceRotation * new Vector3(0, extents.y, -extents.z);
+        corners[21] = center + referenceRotation * new Vector3(0, -extents.y, -extents.z);
+        
+        corners[22] = center + referenceRotation * new Vector3(extents.x, extents.y, 0);
+        corners[23] = center + referenceRotation * new Vector3(-extents.x, extents.y, 0);
+        corners[24] = center + referenceRotation * new Vector3(0, extents.y, extents.z);
+        corners[25] = center + referenceRotation * new Vector3(0, extents.y, -extents.z);
+        corners[26] = center + referenceRotation * new Vector3(extents.x, -extents.y, 0);
+        corners[27] = center + referenceRotation * new Vector3(-extents.x, -extents.y, 0);
+        corners[28] = center + referenceRotation * new Vector3(0, -extents.y, extents.z);
+        corners[29] = center + referenceRotation * new Vector3(0, -extents.y, -extents.z);
+
         return corners;
     }
     
+    private void HandleCursor()
+    {
+        RaycastHit hitCheck;
+        bool raycastCheck = Physics.Raycast(transform.position, mainCam.TransformDirection(Vector3.forward), out hitCheck, dist);
+        if (raycastCheck && hitCheck.transform.CompareTag("pickup")) {
+            mainImage.enabled = false; 
+            secondImage.enabled = true;
+        }
+        else {
+            mainImage.enabled = true;
+            secondImage.enabled = false;
+        }
+    }
+
+    private void HandleResize()
+    {
+        Vector3[] corners = GetObjectPoints(obj);
+        float initialDistance = Vector3.Distance(mainCam.position, obj.transform.position);
+            
+        RaycastHit hit;
+        int i = 0;
+        foreach (Vector3 corner in corners) {
+            direction = (corner - mainCam.position).normalized;
+            if (Physics.Raycast(mainCam.position, direction, out hit, 500.0f, ~(1 << obj.layer))) {
+                distances[i] = Vector3.Distance(mainCam.position, hit.point);
+                i++; 
+            }
+            Debug.DrawLine(mainCam.position, corner + direction * 200.0f, Color.red, 0.1f);
+        }
+            
+        float shortestValue = Mathf.Min(distances);
+        obj.transform.position = 
+            mainCam.position + mainCam.forward * 
+            (shortestValue - Vector3.Distance(obj.transform.position, 
+                corners[Array.IndexOf(distances, shortestValue)]));
+            
+        float scaleFactor = Vector3.Distance(mainCam.position, obj.transform.position) / initialDistance;
+        obj.transform.localScale *= scaleFactor;
+    }
     
-
-
 }
+
--- Assets/scrips/player/PlayerMovement.cs
+++ Assets/scrips/player/PlayerMovement.cs
@@ -4,42 +4,56 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
-    public float playerSpeed;
-    public float jumpHeight;
-    private float gravityValue = -13f;
+    public float moveSpeed = 10f; // movement speed
+    public float maxSpeed = 20f; // maximum speed
+    public float jumpForce = 5f; // jump force
+    public float groundCheckDistance = 1f; // distance from the ground to check for jump
+    public LayerMask groundLayer; // layer for the ground objects
 
-    private void Start()
+    private Rigidbody rb;
+
+    void Start()
     {
-        controller = gameObject.GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+        rb.drag = 0.5f; // increase drag to reduce sliding
+        rb.mass = 1.5f; // increase mass to make player less prone to being pushed around
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
+        // check if player is on the ground
+        bool isGrounded = Physics.Raycast(transform.position, -Vector3.up, groundCheckDistance, groundLayer);
+
+        // get input for movement
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // calculate movement direction
+        Vector3 moveDirection = (moveHorizontal * transform.right + moveVertical * transform.forward).normalized;
+
+        // apply movement force
+        if (moveDirection.magnitude > 0)
         {
-            playerVelocity.y = 0f;
+            rb.AddForce(moveDirection * moveSpeed, ForceMode.VelocityChange);
+
+            // clamp velocity to maximum speed
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+        }
+        else
+        {
+            // set velocity to zero when there is no input
+            rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        move = transform.TransformDirection(move);
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if (move != Vector3.zero)
+        // check if player wants to jump and is on the ground
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            gameObject.transform.forward = move;
+            Jump();
         }
+    }
 
-        // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
